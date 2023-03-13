c3ae54b7570636db42ae8788e8e7bffd185bd696
skanwngud <skanwngud@gmail.com>
2023-03-13 22:51:20 +0900

    2023-03-13

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230312.202313';
+const cacheName = 'chirpy-20230313.225030';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
