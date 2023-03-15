39d8e2ddb3f88f884198fb5b53204d4ec7a0458a
skanwngud <skanwngud@gmail.com>
2023-03-15 22:06:30 +0900

    add to math tag

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230313.225030';
+const cacheName = 'chirpy-20230315.220604';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
