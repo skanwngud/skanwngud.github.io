5b11cc2fd1e78d7cc1c5a27ee1cc26850a05d6c9
skanwngud <skanwngud@gmail.com>
2023-03-20 22:49:24 +0900

    add to sidebar photo

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230315.220941';
+const cacheName = 'chirpy-20230320.224726';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
